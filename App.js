b97beae405ab8cd6250f9aabc24774c2fbe0a1b6
M. Youssef Kassabi <ykassabi@protonmail.com>
2020-02-26 23:19:16 +0100

    featur:add component slider and stepper and date

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AddEntry from './Component/AddEntry';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hi to Me...</Text>
+      <AddEntry />
     </View>
   );
 }
